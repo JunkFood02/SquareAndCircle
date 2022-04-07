8d282cc9b2ae21e51a82bad933b6271fe33c0f89
junkfood <junkfood@hust.edu.cn>
2022-04-07 23:42:07 +0800

    Final

--- main.cpp
+++ main.cpp
@@ -6,12 +6,16 @@
 #include <thread>
 #include <csignal>
 
+int sign = 1;
+
 class MainWindows : public QWidget {
 public:
 
     QPainter *painter;
-private:
+
     void paintEvent(QPaintEvent *) override;
+
+private:
 };
 
 
@@ -23,23 +27,35 @@ void MainWindows::paintEvent(QPaintEvent *) {
     pid_t pid = fork();
 
     if (pid == 0) {
-
-        painter->setBrush(QBrush(Qt::blue, Qt::SolidPattern));
+        if (sign % 2)
+            painter->setBrush(QBrush(Qt::blue, Qt::SolidPattern));
+        else
+            painter->setBrush(QBrush(Qt::green, Qt::SolidPattern));
         qDebug("drawSquare");
         painter->drawRect(20, 20, 160, 160);
     } else {
-
-        painter->setBrush(QBrush(Qt::green, Qt::SolidPattern));
+        if (sign % 2)
+            painter->setBrush(QBrush(Qt::green, Qt::SolidPattern));
+        else
+            painter->setBrush(QBrush(Qt::blue, Qt::SolidPattern));
         qDebug("drawCircle");
         painter->drawEllipse(200, 20, 160, 160);
     }
     painter->end();
 }
 
+void changeColor(MainWindows *w) {
+    for (; sign < 20; sign++) {
+        sleep(1);
+        w->repaint();
+    }
+}
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindows w1;
     w1.show();
+    std::thread th1(changeColor, &w1);
+    th1.detach();
     return QApplication::exec();
 }
